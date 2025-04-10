ad870417c28b4b764525dee2dab9072b43988bd9
Asteski <asteski@outlook.com>
2025-04-10 14:51:38 +0200

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -2,10 +2,7 @@ $searchDirList = @(
     "$env:LOCALAPPDATA\Microsoft\OneDrive\",
     "$env:PROGRAMFILES\Microsoft OneDrive\"
 )
-$newIconList = @(
-    "..\assets\FileSync_537.ico",
-    "..\assets\FileSync_538.ico"
-)
+$newIconList = Get-ChildItem -Path "$PSScriptRoot\..\assets" -Filter "FileSync_*.ico" | ForEach-Object { $_.FullName }
 Stop-Process -n OneDrive -Force
 ForEach ($searchDir in $searchDirList){
     $dllPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 | ForEach-Object { $_.FullName }
