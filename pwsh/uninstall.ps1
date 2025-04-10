5dd8fc993443026e54f12c6d9d0091852144ebf6
Asteski <asteski@outlook.com>
2025-04-10 15:19:52 +0200

    updated install.ps1

--- pwsh/uninstall.ps1
+++ pwsh/uninstall.ps1
@@ -1,12 +1,32 @@
-Stop-Process -n OneDrive -Force
+Write-Host @"
+-----------------------------------------------------------------------
+
+Welcome to OneDrive Better Light Mode Deployment!
+
+Author: Asteski
+GitHub: https://github.com/Asteski/OneDrive-Better-Light-Mode
+
+This is work in progress. You're using this script at your own risk.
+
+-----------------------------------------------------------------------
+
+"@ -ForegroundColor Cyan
+Start-Sleep 1
+for ($a=3; $a -ge 0; $a--) {
+    Write-Host "`rOneDrive Light Mode tray icons will be now restored in $a" -NoNewline -ForegroundColor Yellow
+    Start-Sleep 1
+}
+Write-Host "`r" -NoNewline
+Write-Host "OneDrive Light Mode tray icons restore is starting..." -ForegroundColor Yellow
+Stop-Process -n OneDrive -Force -ErrorAction SilentlyContinue
 $searchDirList = @(
     "$env:LOCALAPPDATA\Microsoft\OneDrive\",
     "$env:PROGRAMFILES\Microsoft OneDrive\"
 )
 ForEach ($searchDir in $searchDirList){
-    $dllPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources.dll" -Recurse | Select-Object -First 1 | ForEach-Object { $_.FullName }
-    $backupPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources_backup.dll" -Recurse | Select-Object -First 1 | ForEach-Object { $_.FullName }
-    if ($backupPath) {
+    $dllPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 | ForEach-Object { $_.FullName }
+    if ($dllPath) {
+        $backupPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources_backup.dll" -Recurse | Select-Object -First 1 | ForEach-Object { $_.FullName }
         Copy-Item -Path $backupPath -Destination $dllPath -Force -ErrorAction SilentlyContinue
         Remove-Item $backupPath -Force -ErrorAction SilentlyContinue
         Start-Sleep -Seconds 1
