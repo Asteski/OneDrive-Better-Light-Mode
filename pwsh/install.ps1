da9dfc3545f48b72e73e01fe39c9bd2243dae153
Asteski <asteski@outlook.com>
2025-04-10 15:15:10 +0200

    retest iconslist env

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -13,7 +13,7 @@ This is work in progress. You're using this script at your own risk.
 "@ -ForegroundColor Cyan
 Start-Sleep 1
 for ($a=3; $a -ge 0; $a--) {
-    Write-Host "`rOneDrive Light Mode tray icons will be now applied in $a." -NoNewline -ForegroundColor Yellow
+    Write-Host "`rOneDrive Light Mode tray icons will be now applied in $a" -NoNewline -ForegroundColor Yellow
     Start-Sleep 1
 }
 Write-Host "`r" -NoNewline
@@ -22,7 +22,11 @@ $searchDirList = @(
     "$env:LOCALAPPDATA\Microsoft\OneDrive\",
     "$env:PROGRAMFILES\Microsoft OneDrive\"
 )
-$newIconList = Get-ChildItem -Path "$PSScriptRoot\..\assets" -Filter "FileSync_*.ico" | ForEach-Object { $_.FullName }
+# $newIconList = Get-ChildItem -Path "$PSScriptRoot\..\assets" -Filter "FileSync_*.ico" | ForEach-Object { $_.FullName }
+$newIconList = @(
+    "..\config\onedrive\FileSync_537.ico",
+    "..\config\onedrive\FileSync_538.ico"
+)
 Stop-Process -n OneDrive -Force -ErrorAction SilentlyContinue
 ForEach ($searchDir in $searchDirList){
     $dllPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 | ForEach-Object { $_.FullName }
