432259b9773a669a906c7e7d4fec4004b5a75e78
Asteski <asteski@outlook.com>
2025-04-10 18:19:58 +0200

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -18,7 +18,7 @@ for ($a=3; $a -ge 0; $a--) {
     Start-Sleep 1
 }
 Write-Host "`r" -NoNewline
-Write-Host "OneDrive Light Mode tray icons deployment is starting..." -ForegroundColor Yellow
+Write-Host "OneDrive Light Mode tray icons deployment is starting..." -ForegroundColor Green
 Stop-Process -n OneDrive -Force
 $searchDirList = @(
     "$env:LOCALAPPDATA\Microsoft\OneDrive\",
@@ -28,8 +28,12 @@ $newIconList = Get-ChildItem -Path "$PSScriptRoot\..\assets" -Filter "FileSync_*
 ForEach ($searchDir in $searchDirList){
     $dllPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources.dll" -Recurse | Select-Object -First 1 | ForEach-Object { $_.FullName }
     if ($dllPath) {
+        Write-Host "`nFound $dllPath" -ForegroundColor DarkYellow
+        Write-Host "Backing up $dllPath" -ForegroundColor DarkYellow
         $backupPath = $dllPath.Replace([System.IO.Path]::GetExtension($dllPath), "_backup" + [System.IO.Path]::GetExtension($dllPath))
+        Write-Host "Backup path: $backupPath" -ForegroundColor DarkYellow
         Copy-Item -Path $dllPath -Destination $backupPath -Force
+        Write-Host "Applying new icons..." -ForegroundColor DarkYellow
         ForEach ($newIconPath in $newIconList) {
             $iconGroup = "ICONGROUP," + [int]([regex]::Match($newIconPath, '_(\d+)\.ico$').Groups[1].Value)
             ..\bin\ResourceHacker.exe -open $dllPath -save $dllPath -action addoverwrite -res $newIconPath -mask $iconGroup
@@ -38,4 +42,4 @@ ForEach ($searchDir in $searchDirList){
     }
 }
 Start-Sleep 1
-Write-Host "`nOneDrive Light Mode tray icons have been applied. Please restart OneDrive." -ForegroundColor Green
+Write-Host "`nOneDrive Light Mode tray icons have been applied. Please restart OneDrive." -ForegroundColor Cyan
