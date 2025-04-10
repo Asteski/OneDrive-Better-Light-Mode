1334440f55736924ad1a4deb5a602d9bb642f1cd
Asteski <asteski@outlook.com>
2025-04-10 14:41:55 +0200

    updated icon names

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -10,7 +10,7 @@ Stop-Process -n OneDrive -Force
 ForEach ($searchDir in $searchDirList){
     $dllPath = Get-ChildItem -Path $searchDir -Filter "FileSync.Resources.dll" -Recurse | Select-Object -First 1 | ForEach-Object { $_.FullName }
     $backupPath = $dllPath.Replace([System.IO.Path]::GetExtension($dllPath), "_backup" + [System.IO.Path]::GetExtension($dllPath))
-    Copy-Item -Path $dllPath -Destination $backupPath -Force -ErrorAction SilentlyContinue > $null 2>&1
+    Copy-Item -Path $dllPath -Destination $backupPath -Force -ErrorAction SilentlyContinue
     ForEach ($newIconPath in $newIconList) {
         $iconGroup = "ICONGROUP," + [int]([regex]::Match($newIconPath, '_(\d+)\.ico$').Groups[1].Value)
         pwsh -NoProfile -Command "..\bin\resourcehacker.exe -open `"$dllPath`" -save `"$dllPath`" -action addoverwrite -res `"$newIconPath`" -mask $iconGroup"
